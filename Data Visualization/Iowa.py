d7a3874138bde20d4a09c004e6bb4026377cac53
Tim Coutinho <tmcoutinho42@gmail.com>
2017-12-06 00:41:53 -0800

    Switched from default file parsing to csv

--- Data Visualization/Iowa.py
+++ Data Visualization/Iowa.py
@@ -4,36 +4,31 @@
 """
 
 from mpl_toolkits.basemap import Basemap
+import csv
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('iowa_cities.txt',index_col='name')
-#print(df.head())
 # m = Basemap(projection='mill', llcrnrlat=40, llcrnrlon=-97, urcrnrlat=44, urcrnrlon=-90)
 # m.drawcoastlines()
 # m.drawcountries()
 # m.drawstates(color='k')
+# cities, pops, lats, lons = [], [], [], []
+# with open('iowa_cities.txt', 'r') as f:
+# 	reader = csv.reader(f)
+# 	next(reader)
+# 	for row in reader:
+# 		if 'zip_code' not in row:
+# 			cities.append(row[1])
+# 			lats.append(float(row[7]))
+# 			lons.append(float(row[8]))
+# 			pops.append(float(row[10]))
 
-cities, pops, lats, lons = [], [], [], []
+# for n, cities in enumerate(cities):
+# 	if pops[n] > 50000:
+# 		xpt, ypt = m(lons[n], lats[n])
+# 		m.plot(xpt, ypt, 'co', markersize=5)
 
-readfile = open('iowa_cities.txt', 'r')
-
-for row in readfile:
-	if 'zip_code' not in row:
-		row = row.split(',')
-		cities.append(row[1])
-		lats.append(float(row[7]))
-		lons.append(float(row[8]))
-		pops.append(float(row[10]))
-
-
-
-for n, cities in enumerate(cities):
-	pass
-	#if pops[n] > 50000:
-	#xpt, ypt = m(lons[n], lats[n])
-	#m.plot(xpt, ypt, 'co', markersize=5)
-
+df = pd.read_csv('iowa_cities.txt',index_col='name')
 df.plot.scatter(x='longitude',y='latitude',s=df['population']/500)
-#df.plot.hexbin(x='longitude',y='latitude',gridsize=15,cmap='coolwarm')
+# df.plot.hexbin(x='longitude',y='latitude',gridsize=15,cmap='coolwarm')
 plt.show()

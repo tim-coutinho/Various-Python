d510817a6c54a33eebb32d1e5e73034f91b047a1
Tim Coutinho <tmcoutinho42@gmail.com>
2017-12-07 17:57:09 -0800

    Don't think exception handling was necessary

--- Web Scraping/Reddit_Scraper.py
+++ Web Scraping/Reddit_Scraper.py
@@ -11,7 +11,7 @@ MAX_POSTS = 10
 
 def main():
 	subreddit = input('Subreddit: ')
-	csv_file = open(f'top{str(MAX_POSTS)}{subreddit.capitalize()}.csv', 'w', newline='')
+	csv_file = open(f'top{MAX_POSTS}{subreddit.capitalize()}.csv', 'w', newline='')
 	csv_writer = csv.writer(csv_file)
 	csv_writer.writerow(['Title', 'Link'])
 	with urlopen(f'https://www.reddit.com/r/{subreddit}/') as url:
@@ -19,23 +19,20 @@ def main():
 	i = 0
 
 	posts = site.find_all('div', onclick='click_thing(this)')	# Only the actual posts in the subreddit
-	print(f"\nCurrent top posts in {posts[0]['data-subreddit']}:\n")
+	print(f"\nCurrent top {MAX_POSTS} posts in {posts[0]['data-subreddit']}:\n")
 
 	for post in posts:
-		try:
-			if post.find('span', class_='stickied-tagline') is None:	# Is not a stickied post
-				title, link = (post.find('p', class_='title').a.text, post['data-url'])
-				if '/r/' in link:
-					link = 'https://www.reddit.com' + link
-				print(title, link)
-				csv_writer.writerow([title, link])
-				i += 1
-				if i == MAX_POSTS:
-					return
-		except Exception as e:
-			pass
-	if i == 0:	# Somehow all stickied posts or all errors
-		print('No links found.')
+		if post.find('span', class_='stickied-tagline') is None:	# Is not a stickied post
+			title, link = (post.find('p', class_='title').a.text, post['data-url'])
+			if '/r/' in link:
+				link = 'https://www.reddit.com' + link
+			print(title, link)
+			csv_writer.writerow([title, link])
+			i += 1
+			if i == MAX_POSTS:
+				return
+	if i == 0:	# Somehow all stickied posts
+		print('No posts found.')
 
 	csv_file.close()
 		

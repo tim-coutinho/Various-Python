c881c01541834087f284221e0d85465f66047b9e
Tim Coutinho <tmcoutinho42@gmail.com>
2017-12-07 17:55:00 -0800

    Added writing to csv file

--- Web Scraping/Reddit_Scraper.py
+++ Web Scraping/Reddit_Scraper.py
@@ -10,22 +10,34 @@ from urllib.request import urlopen
 MAX_POSTS = 10
 
 def main():
-	subreddit = input("Subreddit: ")
+	subreddit = input('Subreddit: ')
+	csv_file = open(f'top{str(MAX_POSTS)}{subreddit.capitalize()}.csv', 'w', newline='')
+	csv_writer = csv.writer(csv_file)
+	csv_writer.writerow(['Title', 'Link'])
 	with urlopen(f'https://www.reddit.com/r/{subreddit}/') as url:
 		site = BeautifulSoup(url, 'lxml')
 	i = 0
-	print(f"\nCurrent top posts in {subreddit}:\n")
-	for post in site.find_all('div', onclick="click_thing(this)"):
+
+	posts = site.find_all('div', onclick='click_thing(this)')	# Only the actual posts in the subreddit
+	print(f"\nCurrent top posts in {posts[0]['data-subreddit']}:\n")
+
+	for post in posts:
 		try:
-			if post.find('span', class_='stickied-tagline') is None:
-				print(post.find('p', class_='title').a.text, post['data-url'])
+			if post.find('span', class_='stickied-tagline') is None:	# Is not a stickied post
+				title, link = (post.find('p', class_='title').a.text, post['data-url'])
+				if '/r/' in link:
+					link = 'https://www.reddit.com' + link
+				print(title, link)
+				csv_writer.writerow([title, link])
 				i += 1
 				if i == MAX_POSTS:
 					return
 		except Exception as e:
 			pass
-	if i == 0:
+	if i == 0:	# Somehow all stickied posts or all errors
 		print('No links found.')
+
+	csv_file.close()
 		
 if __name__ == '__main__':
 	main()

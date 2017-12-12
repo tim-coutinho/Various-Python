cbaf0938573b2d4cde2dceb9925699724ec6971c
Tim Coutinho <tmcoutinho42@gmail.com>
2017-12-11 22:17:22 -0800

    Comments, more abstraction

--- Utilities/Music_Rename.py
+++ Utilities/Music_Rename.py
@@ -13,7 +13,8 @@ no_upper = ('the', 'of', 'a', 'an', 'and', 'in', 'but','or',
 			'for', 'nor', 'on', 'at', 'to', 'from', 'by')
 good_ext = ('.aiff', '.ape', '.asf', '.flac', '.mp3', '.mp4', '.mpc',
 			'.ofr', '.oga', '.ogg', '.ogv', '.opus', '.spx', '.tta', '.wv')
-base = '/Users/tmcou/Music/iTunes/iTunes Media/Music'
+roman_nums = ('Ii', 'Iii', 'Iv', 'Vi', 'Vii', 'Viii', 'Ix')
+base = '/Users/tmcou/Music/iTunes/iTunes Media/Music - Copy'
 
 
 def main():
@@ -27,6 +28,7 @@ def main():
 			modify_album(artist, album)
 
 
+# Moves a song from the artist folder to an Unknown Album folder
 def make_unknown(artist, song):
 	os.mkdir('Unknown Album')
 	os.rename(f'{base}/{artist}/{song}',
@@ -34,31 +36,54 @@ def make_unknown(artist, song):
 	return 'Unknown Album'
 
 
+# Modifies all valid audio files in an album
 def modify_album(artist, album):
 	os.chdir(f'{base}/{artist}/{album}')
 	for song in os.listdir():
-		if os.path.splitext(song)[1] in good_ext:  # Valid audio extension
+		song = os.path.splitext(song)
+		if song[1] in good_ext:  # Valid audio extension
 			modify_song(song)
 
 
+# Modifies a song's title and album tags
 def modify_song(song):
-	audio = EasyID3(song)
-	# Removes any leading album identifiers, i.e. 01 and 13 -
-	song_name = re.sub(r'^\d+\-?\d+\.? ?\-? ', '',
-					   os.path.splitext(song)[0]).lower()
-	try:	# In an album, tag exists
-		audio['album'] = ' '.join([word if word in no_upper
-								        else word.capitalize()
-								        for word
-								        in audio['album'][0].lower().split()])
-	except Exception:  # Not in an album, tag does't exist
+	audio = EasyID3(''.join(song))
+	try:					# In an album, tag exists
+		modify_tag(song, audio, 'album')
+	except Exception:		# Not in an album, tag does't exist
 		pass
-	audio['title'] = ' '.join([word if word.lower() in no_upper
-							        else word.capitalize()
-							        for word in song_name.split()])
-	# Capitalize the first word regardless
-	audio['title'] = audio['title'][0][0].upper() + audio['title'][0][1:]
+	# Removes any leading album identifiers, i.e. 01 and 13 -
+	audio['title'] = re.sub(r'^([0-2]?[\d][^\d,]) ?\-?\.? ?\-?', '',
+							song[0].lstrip('0')).lower()
+	modify_tag(song, audio, 'title')
 	audio.save()
+
+
+# Changes a specific tag of a song, either title or album
+def modify_tag(orig, audio, tag):
+	audio[tag] = re.sub(r'(\w+)(/)(\w+)', r'\1 / \3', audio[tag][0])
+	audio[tag] = ' '.join([word if re.sub(r'[:/\-]', '', word.lower())
+						   in no_upper else word.capitalize()
+						   for word in audio[tag][0].split()])
+	if any(word.strip(':') in roman_nums for word in audio[tag][0].split()):
+		for word in audio[tag][0].split():
+			if word.strip(':') in roman_nums:
+				audio[tag] = audio[tag][0].replace(word, word.upper())
+	c = '_'
+	while '_' in audio[tag][0]:
+		c = input(f'What character should replace the _ in {audio[tag][0]}? ')
+		audio[tag] = audio[tag][0].replace('_', c, 1)
+	# Capitalize the first word regardless
+	audio[tag] = audio[tag][0][0].upper() + audio[tag][0][1:]
+	# sub = re.sub(r"[/:\?]", "_", audio[tag][0])
+	# if tag == 'title' and 'album' in audio:  # Rename file to new song title
+	# 	os.rename(f'{base}/{audio["artist"][0]}/{audio["album"][0].replace("/", "_")}/{"".join(orig)}',
+	# 			  f'{base}/{audio["artist"][0]}/{audio["album"][0].replace("/", "_")}/{sub}{orig[1]}')
+	# 	# os.remove(f'{base}/{audio["artist"][0]}/{audio["album"][0].replace("/", "_")}/{"".join(orig)}')
+	# elif tag == 'title':
+	# 	os.rename(f'{base}/{audio["artist"][0]}/Unknown Album/{"".join(orig)}',
+	# 			  f'{base}/{audio["artist"][0]}/Unknown Album/{sub}{orig[1]}')
+	# 	# os.remove(f'{base}/{audio["artist"][0]}/Unknown Album/{"".join(orig)}')
 
 
 if __name__ == '__main__':

8b6df0a6526612982b2354ac5b5aa0789a41a27a
Tim Coutinho <tmcoutinho42@gmail.com>
2017-12-14 23:40:30 -0800

    Spacing

--- API/APItest.py
+++ API/APItest.py
@@ -12,6 +12,7 @@ from constants import key, us_state_abbrev
 yes = ('yes', 'ye', 'y')
 no = ('no', 'n')
 
+
 def main():
 	try:
 		printData()
@@ -23,6 +24,7 @@ def main():
 	print('\nFarewell!')
 	time.sleep(1)
 
+
 def printData():
 	bys = {'city':byCity, 'cities':byCity, 'zip':byZip, 'zips':byZip, 'coord':byCoords,'coords':byCoords, 'coordinates':byCoords}
 	city, state, zip, temp, wind, gust = bys[input("By city, zip, or coords? ").lower()]()	# Uses input to call corresponding function
@@ -30,6 +32,7 @@ def printData():
 	print(f"{city} is currently experiencing {wind} mph winds with gusts up to {gust} miles per hour.")
 	# if input("Show nearby cities' data? ").lower() in yes:	# Probably not feasible
 	# 	showNearby(city, state)
+
 
 def byCity(city=None, state=None):
 	if city == state == None:	# Function called naturally by user
@@ -45,11 +48,13 @@ def byCity(city=None, state=None):
 		    data["current_observation"]["wind_mph"],
 		    data["current_observation"]["wind_gust_mph"])
 
+
 def byZip():
 	zip = input("Enter zip code: ")
 	with urlopen(f'http://api.wunderground.com/api/{key}/geolookup/q/{zip}.json') as url:
 		data = json.loads(url.read().decode())
 	return byCity(data["location"]["city"], data["location"]["state"])	# Needed to pull weather condition data, doesn't exist in zip json
+
 
 def byCoords():
 	lat, lon = input("Enter latitude, longitude: ").replace(' ', '').split(',')
@@ -57,11 +62,13 @@ def byCoords():
 		data = json.loads(url.read().decode())
 	return byCity(data["location"]["city"], data["location"]["state"])	# Needed to pull weather condition data, doesn't exist in coords json
 
+
 # def showNearby(city, state):	# Probably not feasible
 # 	with urlopen(f'http://api.wunderground.com/api/{key}/geolookup/q/{state}/{city}.json') as url:
 # 		data = json.loads(url.read().decode())
 # 	for station in data["location"]["nearby_weather_stations"]["airport"]["station"]:
 # 		byCity(station["city"], station["state"])
+
 
 if __name__ == '__main__':
 	main()

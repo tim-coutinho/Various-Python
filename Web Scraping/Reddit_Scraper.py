150e5e14d807bdcd97c5a98fe9e264b7556adec6
Tim Coutinho <tmcoutinho42@gmail.com>
2017-12-17 21:35:48 -0800

    Comment spacing

--- Web Scraping/Reddit_Scraper.py
+++ Web Scraping/Reddit_Scraper.py
@@ -23,17 +23,20 @@ def main():
 
 	print(f"\nCurrent top {MAX_POSTS} posts in {subreddit}:\n")
 	i = 0
-	for post in posts:	# Can't use enumerate, don't always want to increment i
-		if post.find('span', class_='stickied-tagline') is None:	# Is not a stickied post
-			title, link = (post.find('p', class_='title').a.text, post['data-url'])
-			if '/r/' in link:	# If hosted on reddit, only gives last part of url
+	for post in posts:  # Can't use enumerate, don't always want to increment
+		if post.find('span', class_='stickied-tagline') is None:
+			# Is not a stickied post
+			title, link = (post.find('p', class_='title').a.text,
+						   post['data-url'])
+			if '/r/' in link:
+				# Only gives last part of url if hosted on Reddit
 				link = 'https://www.reddit.com' + link
 			print(title, link)
 			csv_writer.writerow([title, link])
 			i += 1
 			if i == MAX_POSTS:
 				return
-	if i == 0:	# Somehow all stickied posts
+	if i == 0:  # Somehow all stickied posts
 		print('No posts found.')
 
 	csv_file.close()
@@ -42,7 +45,7 @@ def getSubreddit():
 	subreddit = input('Subreddit: ')
 	with urlopen(f'https://www.reddit.com/r/{subreddit}/') as url:
 		site = BeautifulSoup(url, 'lxml')
-	posts = site.find_all('div', onclick='click_thing(this)')	# Only the actual posts in the subreddit
+	posts = site.find_all('div', onclick='click_thing(this)')
 	subreddit = posts[0]['data-subreddit']
 	return (subreddit, site, posts)
 		

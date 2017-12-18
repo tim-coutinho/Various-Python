1277fe6a56c3572f53a7d588a7502c71ff6f2c81
Tim Coutinho <tmcoutinho42@gmail.com>
2017-12-17 21:43:32 -0800

    Shortened line length

--- Web Scraping/Clicks_to_Philosophy.py
+++ Web Scraping/Clicks_to_Philosophy.py
@@ -20,26 +20,32 @@ def main():
 			print('Article does not exist, try again.')
 		else:
 			if score is None:
-				print(f'It looks like there are no clickable links in paragraphs on that page.')
+				print('''It looks like there are no clickable
+						 links in paragraphs on that page.''')
 			elif score >= 0:
-				print(f'It takes {score} steps to get from {raw} to Philosophy.')
+				print(f'''It takes {score} steps to
+						  get from {raw} to Philosophy.''')
 				break
 
 def calculate(article):
 	if article == '/wiki/Philosophy':
 		return 0
 	if article == '/wiki/Alphabet':
-		print("You've reached Alphabet, which means you're stuck in a loop, unfortunately.")
+		print('''You've reached Alphabet, which means
+				 you're stuck in a loop, unfortunately.''')
 		return -1000
 	elif article == '/wiki/Branch':
-		print("You've reached Branch, which means you're stuck in a loop, unfortunately.")
+		print('''You've reached Branch, which means
+			     you're stuck in a loop, unfortunately.''')
 		return -1000
 	with urlopen(f'https://en.wikipedia.org{article}') as url:
 		site = BeautifulSoup(url, 'lxml')
-	print(site.h1.text)		# The name of the article as displayed on the page
+	print(site.h1.text)  # The name of the article as displayed on the page
 	for paragraph in site.find('div', class_='mw-parser-output').find_all('p'):
 		for link in paragraph.find_all('a', recursive=False):
-			if 'cite_note' not in link['href'] and link['href'] is not article:		# Don't want citations or same article links
+			if ('cite_note' not in link['href']
+							  and link['href'] is not article):
+				# Don't want citations or same article links
 				return calculate(link['href']) + 1
 		
 if __name__ == '__main__':

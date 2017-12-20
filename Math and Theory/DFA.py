b23b19b4b64f1d92bc5522b882e6a52e15e60958
Tim Coutinho <tmcoutinho42@gmail.com>
2017-12-20 14:10:46 -0800

    Shortened line length, " -> '

--- Math and Theory/DFA.py
+++ Math and Theory/DFA.py
@@ -1,21 +1,22 @@
-"""
+'''
 	A program that uses a DFA (Deterministic Finite State Automaton)
 	to determine if the given input is a valid double.
 	Tim Coutinho
-"""
+'''
 
 import re
 import sys
 import numpy as np
 
-transTable = np.array([[2,3,9,4],[2,9,6,5],[2,9,9,4],[5,9,9,9],[5,9,6,9],[8,7,9,9],[8,9,9,9],[8,9,9,9],[9,9,9,9]])
+transTable = np.array([[2,3,9,4],[2,9,6,5],[2,9,9,4],[5,9,9,9],
+					   [5,9,6,9],[8,7,9,9],[8,9,9,9],[8,9,9,9],[9,9,9,9]])
 valid = [2, 5, 8]
 
 def hasInvalidAscii(d):
 	return re.search(r'[^0-9eE\.\+-]', d)
 
 def getDoubles():
-	with open(sys.argv[1], 'r') as f:
+	with open(sys.argv[1]) as f:
 		lines = f.read().splitlines()
 	return lines
 

5c2512198868755fab6ad6e6bd2ae1af2fac9019
Tim Coutinho <tmcoutinho42@gmail.com>
2017-12-28 12:32:17 -0800

    Rearranged functions

--- Web Scraping/Clicks_to_Philosophy.py
+++ Web Scraping/Clicks_to_Philosophy.py
@@ -9,24 +9,6 @@ from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
 
-def main():
-	while True:
-		try:
-			raw = input('Article: ')
-			article = '/wiki/' + raw.replace(' ', '_')
-			score = calculate(article)
-		except TypeError:
-			print('Disambiguation page reached or no links found, try again.')
-		except Exception:
-			print('Article does not exist, try again.')
-		else:
-			if score is None:
-				print('''It looks like there are no clickable
-						 links in paragraphs on that page.''')
-			elif score >= 0:
-				print(f'''It takes {score} steps to
-						  get from {raw} to Philosophy.''')
-				break
 
 def calculate(article):
 	if article == '/wiki/Philosophy':
@@ -49,6 +31,27 @@ def calculate(article):
 							and link['href'] is not article):
 				# Don't want citations or same article links
 				return calculate(link['href']) + 1
-		
+
+
+def main():
+	while True:
+		try:
+			raw = input('Article: ')
+			article = '/wiki/' + raw.replace(' ', '_')
+			score = calculate(article)
+		except TypeError:
+			print('Disambiguation page reached or no links found, try again.')
+		except Exception:
+			print('Article does not exist, try again.')
+		else:
+			if score is None:
+				print('''It looks like there are no clickable
+						 links in paragraphs on that page.''')
+			elif score >= 0:
+				print(f'''It takes {score} steps to
+						  get from {raw} to Philosophy.''')
+				break
+
+
 if __name__ == '__main__':
 	main()

dfec75199f138a71c1ceae2e3af3cbe136ab97a9
Tim Coutinho <tmcoutinho42@gmail.com>
2017-12-29 00:21:33 -0800

    Created skeleton testing site

---
+++ Utilities/test_Music_Rename.py
@@ -0,0 +1,20 @@
+"""
+	Testing site for the Music_Rename module.
+	Tim Coutinho
+"""
+
+import unittest
+
+import Music_Rename
+
+
+class TestRename(unittest.testcase):
+
+	def test_modify_song(self):
+		pass
+
+	def test_make_unknown(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()

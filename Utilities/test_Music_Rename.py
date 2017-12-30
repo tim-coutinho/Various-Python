fe7a1a35ac9881281256788511dd9de70e06770e
Tim Coutinho <tmcoutinho42@gmail.com>
2017-12-30 00:33:28 -0800

    Created make_unknown test

--- Utilities/test_Music_Rename.py
+++ Utilities/test_Music_Rename.py
@@ -11,33 +11,39 @@ from mutagen.easyid3 import EasyID3
 
 import Music_Rename
 
+base = '/Users/tmcou/Music/iTunes/iTunes Media/Music - Copy'
 
 class TestRename(unittest.TestCase):
 
 	@classmethod
 	def setUpClass(cls):
-		os.chdir('/Users/tmcou/Music/iTunes/iTunes Media/Music - Copy')
+		os.chdir(base)
 		os.makedirs('Test Artist/Test Album')
-		os.chdir('Test Artist/Test Album')
-		with open('Test song1.mp3', 'w'):
+		os.chdir('Test Artist')
+		with open('Lonely Song.txt', 'w'):
 			pass
-		with open('Test song2.mp3', 'w'):
+		os.chdir('Test Album')
+		with open('Test song 1.txt', 'w'):
 			pass
-		with open('Test song3.mp3', 'w'):
+		with open('Test song 2.txt', 'w'):
 			pass
-		with open('Test song4.mp3', 'w'):
+		with open('Test song 3.txt', 'w'):
+			pass
+		with open('Test song 4.txt', 'w'):
 			pass
 
 	@classmethod
 	def tearDownClass(cls):
-		os.chdir('/Users/tmcou/Music/iTunes/iTunes Media/Music - Copy')
+		os.chdir(base)
 		rmtree('Test Artist')
 
 	def test_modify_song(self):
 		pass
 
 	def test_make_unknown(self):
-		pass
+		Music_Rename.make_unknown(base, 'Test Artist', 'Lonely Song.txt')
+		os.chdir(os.path.join(base, 'Test Artist', 'Unknown Album'))
+		self.assertTrue(os.path.isfile('Lonely Song.txt'))
 
 
 if __name__ == '__main__':

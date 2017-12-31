9c7a8b034df29a355dba722c23c956ecad3786a1
Tim Coutinho <tmcoutinho42@gmail.com>
2017-12-31 15:48:45 -0800

    Made modified tag not global

--- Utilities/Music_Rename.py
+++ Utilities/Music_Rename.py
@@ -2,7 +2,7 @@
 	Utility written to help organize my music files.
 	Lowercases all title/album words in NO_UPPER,
 	moves songs into folders if not in one already.
-	USE MUSIC - COPY FIRST
+	USE MUSIC - COPY FIRST, run tests beforehand.
 	Tim Coutinho
 """
 
@@ -18,7 +18,6 @@ NO_UPPER = ('a', 'an', 'and', 'at', 'but', 'by', 'for', 'from',
 GOOD_EXT = ('.aiff', '.ape', '.asf', '.flac', '.mp3', '.mp4', '.mpc',
 			'.ofr', '.oga', '.ogg', '.ogv', '.opus', '.spx', '.tta', '.wv')
 ROMAN_NUMS = ('Ii', 'Iii', 'Iv', 'Vi', 'Vii', 'Viii', 'Ix')
-modified = 0
 
 re_nums = re.compile(r'^[0-1]?[\d]\W[.\- ]*')
 re_parens = re.compile(r'[\(:\.]+ *[a-z]')
@@ -48,6 +47,7 @@ def make_unknown(base, artist, song):
 
 def modify_album(base, artist, album, individual=False):
 	"""Modifies all valid audio files in an album."""
+	modified = 0
 	if album != 'Unknown Album':
 		print(f'  {album}')
 	try:
@@ -55,6 +55,8 @@ def modify_album(base, artist, album, individual=False):
 	except NotADirectoryError:
 		album = make_unknown(base, artist, album)
 	for song in os.listdir():
+		if os.path.isdir(song):
+			continue
 		with open_audio(song) as audio:
 			if audio:
 				if individual:
@@ -62,20 +64,20 @@ def modify_album(base, artist, album, individual=False):
 					if 'title' in audio:
 						del audio['title']
 				modify_song(base, song, audio)
+				modified += 1
+	return modified
 
 
 def modify_song(base, song, audio):
 	"""Modify a song's title and album tags."""
-	global modified
 	try:
-		audio['album'] = modify_tag(base, song, audio, audio['album'])
-	except KeyError:  # Not in an album, tag does't exist
+		audio['album'] = modify_tag(base, song, audio['album'])
+	except KeyError:  # Not in an album, tag doesn't exist
 		pass
 	if 'title' not in audio:  # Use file name as title
 		# Removes any leading album identifiers, i.e. '01' and '13 -'
 		audio['title'] = re_nums.sub('', song[0].lstrip('0'))
-	audio['title'] = modify_tag(base, song, audio, audio['title'])
-	modified += 1
+	audio['title'] = modify_tag(base, song, audio['title'])
 
 
 def modify_tag(base, orig, tag):
@@ -92,14 +94,14 @@ def modify_tag(base, orig, tag):
 		for word in tag[0].split():
 			if word.strip(':') in ROMAN_NUMS:
 				tag = [tag[0].replace(word, word.upper())]
-	# Underscores (title likely pulled from file name)
-	while '_' in tag[0]:
-		c = input(f'What character should replace the _ in {tag[0]}? ')
-		tag = [tag[0].replace('_', c, 1)]
 	# Parentheses, colons, ellipses
 	matches = re_parens.findall(tag[0])
 	for match in matches:
 		tag = [tag[0].replace(match, match.upper())]
+	# Underscores (title likely pulled from file name)
+	while '_' in tag[0]:
+		c = input(f'What character should replace the _ in {tag[0]}? ')
+		tag = [tag[0].replace('_', c, 1)]
 	# Finally, capitalize the first word regardless
 	tag = [tag[0][0].upper() + tag[0][1:]]
 	return tag
@@ -107,15 +109,17 @@ def modify_tag(base, orig, tag):
 
 def main():
 	base = '/Users/tmcou/Music/iTunes/iTunes Media/Music - Copy'
+	modified = 0
 	os.chdir(base)
 	for artist in os.listdir():
 		print(artist)
 		os.chdir(os.path.join(base, artist))
 		for album in os.listdir():
-			modify_album(base, artist, album)
+			modified += modify_album(base, artist, album)
+	return modified
 
 
 if __name__ == '__main__':
-	main()
-	# modify_album('/Users', 'tmcou', 'Downloads')
+	modified = main()
+	# modified = modify_album('/Users', 'tmcou', 'Downloads')
 	print(f'\nModified {modified} songs.')

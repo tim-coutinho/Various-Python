f3ba6735a27adbdf6cc7bcc9800eb45dbd2627dd
Tim Coutinho <tmcoutinho42@gmail.com>
2018-01-01 17:54:21 -0800

    Moved bulk of code to function

--- Web Scraping/Reddit_Scraper.py
+++ Web Scraping/Reddit_Scraper.py
@@ -11,28 +11,16 @@ from bs4 import BeautifulSoup
 MAX_POSTS = 10
 
 
-def getSubreddit():
+def get_subreddit():
 	subreddit = input('Subreddit: ')
 	with urlopen(f'https://www.reddit.com/r/{subreddit}/') as url:
 		site = BeautifulSoup(url, 'lxml')
 	posts = site.find_all('div', onclick='click_thing(this)')
 	subreddit = posts[0]['data-subreddit']
-	return (subreddit, site, posts)
-		
+	return (subreddit, posts)
 
-def main():
-	while True:
-		try:
-			subreddit, site, posts = getSubreddit()
-			break
-		except KeyError as e:
-			print('Subreddit does not exist, try again.')
 
-	csv_file = open(f'top{MAX_POSTS}{subreddit}.csv', 'w', newline='')
-	csv_writer = csv.writer(csv_file)
-	csv_writer.writerow(['Title', 'Link'])
-
-	print(f"\nCurrent top {MAX_POSTS} posts in {subreddit}:\n")
+def get_posts(posts, csv_writer):
 	i = 0
 	for post in posts:  # Can't use enumerate, don't always want to increment
 		if post.find('span', class_='stickied-tagline') is None:
@@ -40,7 +28,7 @@ def main():
 			title, link = (post.find('p', class_='title').a.text,
 						   post['data-url'])
 			if '/r/' in link:
-				# Only gives last part of url if hosted on Reddit
+				# Only last part of url is given if hosted on Reddit
 				link = 'https://www.reddit.com' + link
 			print(title, link)
 			csv_writer.writerow([title, link])
@@ -49,8 +37,21 @@ def main():
 				return
 	if i == 0:  # Somehow all stickied posts
 		print('No posts found.')
+		
 
-	csv_file.close()
+def main():
+	while True:
+		try:
+			subreddit, posts = get_subreddit()
+			break
+		except KeyError as e:
+			print('Subreddit does not exist, try again.')
+
+	with open(f'top{MAX_POSTS}{subreddit}.csv', 'w', newline='') as f:
+		csv_writer = csv.writer(f)
+		csv_writer.writerow(['Title', 'Link'])
+		print(f"\nCurrent top {MAX_POSTS} posts in {subreddit}:\n")
+		get_posts(posts, csv_writer)
 
 
 if __name__ == '__main__':
